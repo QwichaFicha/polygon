9c89f4d9d351af4940a268d121d020c2533f1348
QwichaFicha <shumakher228@gmail.com>
2025-01-11 18:49:31 +0300

    добавил комментарии к коду

--- lab-9.py
+++ lab-9.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-
 def even_numbers(array):
+    # Преобразуем входной список в массив numpy
     arr = np.array(array)
+    
+    # Находим уникальные элементы и их количество
     unique, counts = np.unique(arr, return_counts=True)
+    
+    # Создаем массив с уникальными элементами и их количеством(для удобства транспонируем)
     arr = np.asarray((unique, counts)).T
+    
+    # Отбираем элементы с чётным количеством вхождений
     res = arr[arr[:, 1] % 2 == 0]
+    
+    # Повторяем элементы с чётным количеством вхождений
     res = np.repeat(res[:, 0], res[:, 1])
+    
     return res
-
 
 print(even_numbers([1, 1, 2, 3, 3, 3]))
